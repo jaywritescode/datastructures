f97fbe7ccf71cbc5edaaca4fc70a3ce1d8da50fd
Jay Harris <jaywritescode@users.noreply.github.com>
2020-08-17 12:29:31 -0400

    switch python disjoint set to union by size

--- python/datastructures/disjoint_set.py
+++ python/datastructures/disjoint_set.py
@@ -1,7 +1,7 @@
 class DisjointSet:
     def __init__(self, values):
         self.indices = {v: k for k, v in enumerate(values)}
-        self.repr = [-1] * len(values)
+        self.forest = [-1] * len(values)
 
     def find(self, value):
         """
@@ -14,20 +14,14 @@ class DisjointSet:
         """
         if value not in self.indices:
             raise KeyError
-        pass
-
-        index = self.indices[value]
-
-        # in the union implementation, if index points to a root node, then the value stored at self.repr[index] is the
-        # height of the root node's tree times -1
-        return index if self.repr[index] < 0 else self._find(index)
+        return self._find(self.indices[value])
 
     def _find(self, index):
-        if self.repr[index] < 0:
+        if self.forest[index] < 0:
             return index
         else:
-            self.repr[index] = self._find(self.repr[index])
-            return self.repr[index]
+            self.forest[index] = self._find(self.forest[index])
+            return self.forest[index]
 
     def union(self, value1, value2):
         """
@@ -37,18 +31,16 @@ class DisjointSet:
         :param value2: another value
         :return: None
         """
-        root1 = self.find(value1)
-        root2 = self.find(value2)
+        return self._union_by_size(self.find(value1), self.find(value2))
 
+    def _union_by_size(self, root1, root2):
         if root1 == root2:
             return
 
-        # keep the tree depth small by pointing the root of the smaller tree to the root of the larger tree
-        if self.repr[root1] < self.repr[root2]:
-            self.repr[root1] = root2
+        # make the tree with fewer nodes a child of the tree with more nodes
+        if self.forest[root2] < self.forest[root1]:
+            self.forest[root2] += self.forest[root1]
+            self.forest[root1] = root2
         else:
-            # if we merge two trees of the same height, the resulting tree will have a height that is one greater than
-            # either of the merged trees
-            if self.repr[root1] == self.repr[root2]:
-                self.repr[root1] -= 1
-            self.repr[root2] = root1
+            self.forest[root1] += self.forest[root2]
+            self.forest[root2] = root1
--- python/tests/test_disjoint_set.py
+++ python/tests/test_disjoint_set.py
@@ -3,29 +3,41 @@ from datastructures.disjoint_set import DisjointSet
 
 
 class TestDisjointSet(unittest.TestCase):
+    def setUp(self):
+        self.disjoint_set = DisjointSet(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+
     def test_find(self):
-        array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        self.disjoint_set.forest = [-1, -1, -1, -1, -4, 4, 4, 6]
 
-        disjoint_set = DisjointSet(array)
+        self.assertEqual(self.disjoint_set.find('f'), self.disjoint_set.find('h'))
+        self.assertNotEqual(self.disjoint_set.find('c'), self.disjoint_set.find('g'))
 
-        # initially, all elements are in different sets
-        self.assertNotEqual(disjoint_set.find('c'), disjoint_set.find('g'))
+    def test_path_compression(self):
+        disjoint_set = DisjointSet(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
+        disjoint_set.forest = [-16, 0, 0, 2, 0, 4, 4, 6, 0, 8, 8, 10, 8, 12, 12, 14]
 
-        disjoint_set.repr = [-1, -1, -1, -1, -1, 4, 4, 6]
-
-        # now, {4, 5, 6, 7} is a set.
-        self.assertEqual(disjoint_set.find('f'), disjoint_set.find('h'))
+        disjoint_set.find('o')
+        self.assertListEqual([-16, 0, 0, 2, 0, 4, 4, 6, 0, 8, 8, 10, 0, 12, 0, 14], disjoint_set.forest)
 
     def test_union(self):
-        array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        self.disjoint_set.forest = [-1, -1, -1, -1, -4, 4, 4, 6]
+        self.disjoint_set.union('d', 'e')
 
-        disjoint_set = DisjointSet(array)
+        self.assertEqual(self.disjoint_set.find('d'), self.disjoint_set.find('e'))
 
-        disjoint_set.union('e', 'f')
-        disjoint_set.union('g', 'h')
-        disjoint_set.union('e', 'g')
+    def test_union_by_size(self):
+        self.disjoint_set.forest = [-1, -1, -1, -1, -4, 4, 4, 6]
+        self.disjoint_set.union('d', 'g')
 
-        self.assertEqual(disjoint_set.find('e'), disjoint_set.find('h'))
+        self.assertListEqual([-1, -1, -1, 4, -5, 4, 4, 6], self.disjoint_set.forest)
+
+        # attempt to merge two elements in the same set
+        self.disjoint_set.union('f', 'g')
+        self.assertListEqual([-1, -1, -1, 4, -5, 4, 4, 6], self.disjoint_set.forest)
+
+        # merge two sets of equal size
+        self.disjoint_set.union('a', 'c')
+        self.assertTrue(self.disjoint_set.forest in [[-2, -1, 0, 4, -5, 4, 4, 6], [2, -1, -2, 4, -5, 4, 4, 6]])
 
 
 if __name__ == '__main__':
